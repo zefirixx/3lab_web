f6e169f0dcad24d5cb2efc5bcd1bd2d8a254f079
зефирка3 <catsunsety@gmail.com>
2026-02-14 22:58:45 +0200

    Update index.php

--- php-f-s-lab/www/index.php
+++ php-f-s-lab/www/index.php
@@ -1,5 +1,15 @@
 <?php session_start(); ?>
 
+<?php if(isset($_SESSION['errors'])): ?>
+    <ul style="color:red;">
+        <?php foreach($_SESSION['errors'] as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php unset($_SESSION['errors']); ?>
+<?php endif; ?>
+
+
 <?php if(isset($_SESSION['formData'])): ?>
     <p>Данные из сессии:</p>
     <ul>
